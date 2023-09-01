8fe8ef0e87efad6aebec94f53617d6561975d220
Water-Stone <wngjsthd8@naver.com>
2023-09-01 15:24:18 +0900

    logout js modified

--- js/logout.js
+++ js/logout.js
@@ -1,19 +1,47 @@
-const signupButton = document.querySelector(".logout-btn");
+const logoutbtn = document.querySelector(".logoutbtn");
 
-signupButton.addEventListener("click", function () {
-    const token = localStorage.getItem("token");
-    axios
-        .post(BaseUrl + "/user/logout/", {
-            headers: {
-                Authorization: `Bearer ${token}`, // Add the token to the request headers
-            },
-        })
+logoutbtn.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    fetch(BaseUrl + "/user/logout/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+        body: JSON.stringify({
+            refresh: localStorage.getItem("refreshToken"),
+        }),
+    })
         .then((response) => {
-            // handle success
-            window.location.href = "../index.html";
+            if (!response.ok) throw new Error(response.status);
+
+            localStorage.removeItem("token");
+            localStorage.removeItem("refreshToken");
+
+            window.location.href = "./login.html";
         })
         .catch((error) => {
-            // handle error
+            console.error("Error:", error);
+
             alert(`로그아웃 실패: ${error.message}`);
         });
 });
+
+// logoutbtn.addEventListener("click", function () {
+//     const token = localStorage.getItem("token");
+//     axios
+//         .post(BaseUrl + "/user/logout/", {
+//             headers: {
+//                 Authorization: `Bearer ${token}`, // Add the token to the request headers
+//             },
+//         })
+//         .then((response) => {
+//             // handle success
+//             window.location.href = "../index.html";
+//         })
+//         .catch((error) => {
+//             // handle error
+//             alert(`로그아웃 실패: ${error.message}`);
+//         });
+// });
