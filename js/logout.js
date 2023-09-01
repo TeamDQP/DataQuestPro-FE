c4411a269cc0d41c653ec1fbcfe8757034fea353
Water-Stone <wngjsthd8@naver.com>
2023-09-01 13:58:10 +0900

    add logout

---
+++ js/logout.js
@@ -0,0 +1,20 @@
+const BaseUrl = "http://15.164.56.233";
+const signupButton = document.querySelector(".logout-btn");
+
+signupButton.addEventListener("click", function () {
+    const token = localStorage.getItem("token");
+    axios
+        .get(BaseUrl + "/user/logout/", {
+            headers: {
+                Authorization: `Bearer ${token}`, // Add the token to the request headers
+            },
+        })
+        .then((response) => {
+            // handle success
+            window.location.href = "../index.html";
+        })
+        .catch((error) => {
+            // handle error
+            alert(`로그아웃 실패: ${error.message}`);
+        });
+});
