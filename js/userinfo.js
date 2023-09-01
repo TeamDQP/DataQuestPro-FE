3f0131bd82872f86ce02cd1157863327670b5538
Water-Stone <wngjsthd8@naver.com>
2023-09-01 13:56:29 +0900

    오타 수정

--- js/userinfo.js
+++ js/userinfo.js
@@ -1,10 +1,9 @@
-const BaseUrl = 'http://15.164.56.233';
-const token = localStorage.getItem('token');
-a;
+const BaseUrl = "http://15.164.56.233";
+const token = localStorage.getItem("token");
 
 function getUserinfo() {
     axios
-        .get(BaseUrl + '/user/update/', {
+        .get(BaseUrl + "/user/update/", {
             headers: {
                 Authorization: `Bearer ${token}`,
             },
@@ -14,16 +13,16 @@ function getUserinfo() {
             const email = response.data.email;
             const email_opt_in = response.data.email_opt_in;
 
-            document.getElementById('name').innerText = name;
-            document.getElementById('email').innerText = email;
-            document.getElementById('email_opt_in').innerText = email_opt_in;
+            document.getElementById("name").innerText = name;
+            document.getElementById("email").innerText = email;
+            document.getElementById("email_opt_in").innerText = email_opt_in;
 
             if (email_opt_in) {
-                document.getElementById('email_opt_in').innerText =
-                    '수신 동의함';
+                document.getElementById("email_opt_in").innerText =
+                    "수신 동의함";
             } else {
-                document.getElementById('email_opt_in').innerText =
-                    '수신 거부함';
+                document.getElementById("email_opt_in").innerText =
+                    "수신 거부함";
             }
         })
         .catch((error) => {
