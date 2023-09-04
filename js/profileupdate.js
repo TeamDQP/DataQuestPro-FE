39762ddcbb188fa300b818a8c222ff1f3260bf84
Water-Stone <wngjsthd8@naver.com>
2023-09-04 11:21:40 +0900

    profile update redirect to profile page

--- js/profileupdate.js
+++ js/profileupdate.js
@@ -1,8 +1,8 @@
-const BaseUrl = 'http://15.164.56.233';
-const token = localStorage.getItem('token');
+const BaseUrl = "http://15.164.56.233";
+const token = localStorage.getItem("token");
 
-const imgForm = document.getElementById('profile-img-form');
-imgForm.addEventListener('submit', function (event) {
+const imgForm = document.getElementById("profile-img-form");
+imgForm.addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
     // Get form data
     const formData = new FormData(imgForm);
@@ -10,13 +10,14 @@ imgForm.addEventListener('submit', function (event) {
     // formData.append('profileimage','')
     // Make API call using Axios
     axios
-        .post(BaseUrl + '/user/profile/update/', formData, {
+        .post(BaseUrl + "/user/profile/update/", formData, {
             headers: {
                 Authorization: `Bearer ${token}`, // Add the token to the request headers
             },
         })
         .then((response) => {
             console.log(response);
+            window.location.href = "./profilepage.html";
         })
         .catch((error) => {
             console.log(error);
@@ -25,7 +26,7 @@ imgForm.addEventListener('submit', function (event) {
 
 function getProfile() {
     axios
-        .get(BaseUrl + '/user/profile/update/', {
+        .get(BaseUrl + "/user/profile/update/", {
             headers: {
                 Authorization: `Bearer ${token}`, // Add the token to the request headers
             },
@@ -39,15 +40,15 @@ function getProfile() {
             console.log(username);
             console.log(profileimage);
 
-            const imgItem = document.createElement('img');
-            imgItem.classList.add('thumbnail');
-            imgItem.setAttribute('alt', 'image unavailable');
-            imgItem.setAttribute('src', `${BaseUrl}${profileimage}`);
-            const image = document.querySelector('.display-img');
+            const imgItem = document.createElement("img");
+            imgItem.classList.add("thumbnail");
+            imgItem.setAttribute("alt", "image unavailable");
+            imgItem.setAttribute("src", `${BaseUrl}${profileimage}`);
+            const image = document.querySelector(".display-img");
             image.append(imgItem);
 
             // document.getElementById('image').src = `http://127.0.0.1:8000${profileimage}`;
-            document.getElementById('username').value = username;
+            document.getElementById("username").value = username;
 
             //     // 이미지 요소 생성하여 가져온 profileimage 주소 넣어줌
             //     const imgItem = document.createElement("img");
